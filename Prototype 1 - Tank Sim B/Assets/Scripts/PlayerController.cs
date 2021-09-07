7529677eca4d00eb670003db93a77907c8b23a42
Powell <preston.powell@nebo.edu>
2021-09-07 09:21:44 -0600

    Worked on Prototype 1

--- Prototype 1 - Tank Sim B/Assets/Scripts/PlayerController.cs
+++ Prototype 1 - Tank Sim B/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f; 
     public float turnSpeed;
+    // Left and Rigth Input
+    public float hInput;
+    //Forward and Back Input
+    public float vInput;
+
     // projectile
 
     // Start is called before the first frame update
@@ -17,6 +22,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        // Getting button press values for Horizontal and Vertical Inputs
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        
+        // Makes the vehicle go left and right
+        transform.Rotate(Vector3.up, turnSpeed * hInput * Time.deltaTime);
+        // Makes the vehicle go forward and back
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);
     }
 }
